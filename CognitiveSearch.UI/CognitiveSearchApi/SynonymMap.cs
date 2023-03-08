e55e7476834baa4b340c669e653a06c7d5eb39b0
Damon DeSonier <ddesonier@microsoft.com>
2023-03-08 15:24:29 -0500

    Initial

---
+++ CognitiveSearch.UI/CognitiveSearchApi/SynonymMap.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace CognitiveSearch.UI.CognitiveSearchApi
+{
+    public class SynonymMapList
+    {
+        [JsonPropertyName("@odata.context")]
+        public string context { get; set; }
+
+        public SynonymMap[] value { get; set; }
+    }
+
+    public class EncryptionKey
+    {
+        public string keyVaultKeyName;
+        public string keyVaultKeyVersion;
+        public string keyVaultUri;
+    }
+
+
+    public class SynonymMap
+    {
+        [Required()]
+        public string name { get; set; }
+
+        public string format
+        {
+            //currently solr is the only accepted format
+            get { return "solr"; }
+        }
+
+        [Required()]
+        public string synonyms { get; set; }
+
+        public EncryptionKey encryptionKey { get; set; }
+
+        [IgnoreDataMember]
+        public bool isActive { get; set; }
+    }
+}
